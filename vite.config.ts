3fe7d908eafde0e946857cc52c2c768f3871d520
Sebastian Saethre <sebastian.saethre@gmail.com>
2025-06-18 04:22:55 +0200

    Add vite-plugin-sitemap for automatic sitemap.xml generation

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import sitemap from 'vite-plugin-sitemap';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), sitemap({
+    hostname: 'https://hovdenmusikklubb.no',
+  })],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
